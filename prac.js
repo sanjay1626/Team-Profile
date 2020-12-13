7e11a141cd459ebeb87ea44df4c507df7e54d232
Sanjay Gonsalves <snjgonsalves@gmail.com>
2020-12-12 20:16:14 -0800

    Made changes in code structure

--- prac.js
+++ prac.js
@@ -19,39 +19,39 @@ const { get } = require("http");
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 const employees = [];
-const employeesId=[];
+const employeesId = [];
 
 
 
 //Function created to add team members
 
 function createTeam() {
-            
+
     function createManager() {
         inquirer.prompt([
             {
                 type: "input",
                 name: "managerName",
-                message:"Manager Name"
+                message: "Manager Name"
             },
             {
                 type: "input",
                 name: "managerId",
-                message:"Manager Id",
-                validate: answer => isNaN(parseInt(answer)) ? 'Not a number!, please re-enter a valid Id': true
+                message: "Manager Id",
+                validate: answer => isNaN(parseInt(answer)) ? 'Not a number!, please re-enter a valid Id' : true
             },
             {
                 type: "input",
                 name: "managerEmail",
-                message:"Manager Email"
+                message: "Manager Email"
             },
             {
                 type: "input",
                 name: "managerOfficeNumber",
-                message:"Manager Office Number",
-                validate: answer => isNaN(parseInt(answer)) ? 'Not a valid office number!, please re-enter a office number': true
+                message: "Manager Office Number",
+                validate: answer => isNaN(parseInt(answer)) ? 'Not a valid office number!, please re-enter a office number' : true
             }
-        ]).then(answer=> {
+        ]).then(answer => {
             const manager = new Manager(answer.managerName, answer.managerId, answer.managerEmail, answer.mangerOfficeNumber);
             employees.push(manager);
             employeesId.push(answer.managerId);
@@ -67,8 +67,8 @@ function createTeam() {
                 message: "choose type of member",
                 choices: ["Engineer", "Intern", "I am finished"]
             }
-        ]).then(choicer=> {
-            switch(choicer.memberChoice) {
+        ]).then(choicer => {
+            switch (choicer.memberChoice) {
                 case "Engineer":
                     createEngineer();
                     break;
@@ -83,33 +83,34 @@ function createTeam() {
 
     }
 
-    function createEngineer(){
+    function createEngineer() {
         inquirer.prompt([
             {
                 type: "input",
                 name: "engineerName",
-                message:"Engineer Name"
+                message: "Engineer Name"
             },
             {
                 type: "input",
                 name: "engineerId",
-                message:"Engineer Id",
-                validate: answer => isNaN(parseInt(answer)) ? 'Not a number!, please re-enter a valid Id': true                
+                message: "Engineer Id",
+                validate: answer => isNaN(parseInt(answer)) ? 'Not a number!, please re-enter a valid Id' : true,
+
             },
             {
                 type: "input",
                 name: "engineerEmail",
-                message:"Engineer Email"
+                message: "Engineer Email"
             },
             {
                 type: "input",
                 name: "engineerOfficeNumber",
-                message:"Engineer Office Number",
-                validate: answer => isNaN(parseInt(answer)) ? 'Not a valid office number!, please re-enter a office number': true
+                message: "Engineer Office Number",
+                validate: answer => isNaN(parseInt(answer)) ? 'Not a valid office number!, please re-enter a office number' : true
             }
-        ]).then(answer=> {
+        ]).then(answer => {
             const engineer = new Engineer(answer.engineerName, answer.engineerId, answer.engineerEmail, answer.engineerOfficeNumber);
-            
+
             employees.push(engineer);
             employeesId.push(answer.engineerId);
             createTeam();
@@ -117,31 +118,31 @@ function createTeam() {
 
     }
 
-    function createIntern(){
+    function createIntern() {
         inquirer.prompt([
             {
                 type: "input",
                 name: "internName",
-                message:"Intern Name"
+                message: "Intern Name"
             },
             {
                 type: "input",
                 name: "internId",
-                message:"Intern ID",
-                validate: answer => isNaN(parseInt(answer)) ? 'Not a number!, please re-enter a valid Id': true
+                message: "Intern ID",
+                validate: answer => isNaN(parseInt(answer)) ? 'Not a number!, please re-enter a valid Id' : true
             },
             {
                 type: "input",
                 name: "internEmail",
-                message:"Intern Email"
+                message: "Intern Email"
             },
             {
                 type: "input",
                 name: "internOfficeNumber",
-                message:"Intern Office Number",
-                validate: answer => isNaN(parseInt(answer)) ? 'Not a valid office number!, please re-enter a office number': true
+                message: "Intern Office Number",
+                validate: answer => isNaN(parseInt(answer)) ? 'Not a valid office number!, please re-enter a office number' : true
             }
-        ]).then(answer=> {
+        ]).then(answer => {
             const intern = new Intern(answer.internName, answer.internId, answer.internEmail, answer.internOfficeNumber);
             employees.push(intern);
             employeesId.push(answer.internId);
@@ -150,7 +151,7 @@ function createTeam() {
 
     }
 
-    function renderHTML(){
+    function renderHTML() {
         fs.writeFileSync(outputPath, render(employees), "utf-8");
     }
 
